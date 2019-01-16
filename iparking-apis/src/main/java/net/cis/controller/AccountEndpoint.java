96004e96d2b29f65b056494e9170aa143d56e24d
tranthoai151 <tranthoai142@gmail.com>
2019-01-16 17:06:11 +0700

    update user and create

--- iparking-apis/src/main/java/net/cis/controller/AccountEndpoint.java
+++ iparking-apis/src/main/java/net/cis/controller/AccountEndpoint.java
@@ -3,12 +3,10 @@ package net.cis.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,13 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.cis.common.util.Utils;
 import net.cis.common.web.BaseEndpoint;
-import net.cis.constants.ResponseErrorCodeConstants;
-import net.cis.dto.AccountUserDto;
 import net.cis.dto.ErrorDto;
 import net.cis.dto.ResponseApi;
 import net.cis.dto.UserDto;
-import net.cis.jpa.entity.UserEntity;
 import net.cis.service.UserService;
 
 @RestController
@@ -62,36 +58,55 @@ public class AccountEndpoint extends BaseEndpoint {
 
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date date = new Date();
-		
+
 		ResponseApi endpoint = new ResponseApi();
 		ErrorDto errorDto = new ErrorDto();
-		
-		
- 
-		
-		String createdAt = formatter.format(date);
-		
-		
-		//check username da ton tai
-		UserEntity user = userService.findByUsername(username);
-		if (user != null) {
-			errorDto.setCode(ResponseErrorCodeConstants.StatusBadRequest);
-			errorDto.setMessage("username đã tồn tại!");
-			endpoint.setError(errorDto);
-			endpoint.setData(null);
-		}else {
-			UserDto userDto = new UserDto();
-			userDto.setFullname(fullname);
-			userDto.setUsername(username);
-			userDto.setStatus(1);
-			userDto.setCreated_at(createdAt);
-			userDto.setUpdated_at(createdAt);
-			userDto.setChecksum(1233456777);
-			userDto.setPwd_counter(5);
-			userDto.setType(2);
-			endpoint = userService.save(userDto);
-		}
 
+		String createdAt = formatter.format(date);
+
+		UserDto userDto = new UserDto();
+		userDto.setFullname(fullname);
+		userDto.setUsername(username);
+		userDto.setStatus(1);
+		userDto.setCreated_at(createdAt);
+		userDto.setUpdated_at(createdAt);
+		userDto.setPwd_counter(5);
+		userDto.setType(2);
+		userDto.setPassword(password);
+		userDto.setPhone(phone);
+		userDto.setEmail(email);
+		endpoint = userService.save(userDto);
+
+		return endpoint;
+
+	}
+
+	// update Account User
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	@ApiOperation(" account user")
+	public @ResponseBody Object updateAccountUser(@RequestParam(name = "username", required = true) String username,
+			@RequestParam(name = "fullname", required = true) String fullname,
+			@RequestParam(name = "email", required = true) String email,
+			@RequestParam(name = "phone", required = true) String phone,
+			@RequestParam(name = "password", required = true) String password,
+			@RequestParam(name = "role", required = true) int role, @RequestParam(name = "id", required = true) int id)
+			throws Exception {
+
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date date = new Date();
+
+		String updateAt = formatter.format(date);
+
+		UserDto userDto = new UserDto();
+		userDto.setFullname(fullname);
+		userDto.setUsername(username);
+		userDto.setUpdated_at(updateAt);
+		userDto.setPassword(password);
+		userDto.setPhone(phone);
+		userDto.setEmail(email);
+		userDto.setId(id);
+		
+		ResponseApi endpoint = userService.update(userDto);
 
 		return endpoint;
 
