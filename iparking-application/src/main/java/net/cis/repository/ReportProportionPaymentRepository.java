29fcf2c122b712f11af1b8621757ab8f2c80221e
liemnh <liem.nguyen@cis.net.vn>
2019-01-18 09:00:13 +0700

    refact  code

--- iparking-application/src/main/java/net/cis/repository/ReportProportionPaymentRepository.java
+++ iparking-application/src/main/java/net/cis/repository/ReportProportionPaymentRepository.java
@@ -16,8 +16,8 @@ public interface ReportProportionPaymentRepository extends JpaRepository<ReportP
 	List<ReportProportionPaymentDto> findByProportionPaymentEntity(@Param("cppId") List<Long> cppId,
 			@Param("fromDate") long fromDate, @Param("toDate") long toDate);
 
-	@Query(value = "Select ticket from ReportProportionPaymentEntity ticket where (parkingId =:cppId) AND ( day >= :fromDate AND day <= :toDate ) GROUP BY parkingCode")
-	ReportProportionPaymentEntity findByProportionPaymentEntity(@Param("cppId") Long cppId,
+	@Query(value = "Select new net.cis.dto.ReportProportionPaymentDto(rpp.id as id,rpp.parkingCode as parkingCode, rpp.company as company ,sum(rpp.revenue) as revenue,sum(rpp.sms) as sms, sum(rpp.atm) as atm, sum(rpp.balance) as balance, sum(rpp.balance_tth) as balance_tth, sum(rpp.visa_master_kh) as visa_master_kh, sum(rpp.visa_master_tth) as visa_master_tth,sum(rpp.cash) as cash ,rpp.parkingId as parkingId ,rpp.companyId as companyId, cpp.address as address , cpp.capacity as capacity ) from ReportProportionPaymentEntity rpp INNER JOIN ParkingEntity cpp ON rpp.parkingId = cpp.oldId  where (rpp.parkingId in :cppId) AND ( rpp.day >= :fromDate AND rpp.day <= :toDate ) GROUP BY rpp.parkingCode")
+	ReportProportionPaymentDto findByProportionPaymentEntity(@Param("cppId") Long cppId,
 			@Param("fromDate") long fromDate, @Param("toDate") long toDate);
 
 }
